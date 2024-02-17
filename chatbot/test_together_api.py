cbdaf90a38cc53d4eb6c28052355553c70e4d4c7
gr125 <gaurirenjith5@gmail.com>
2024-02-17 12:41:42 -0800

    add requests for additional patient questions, organize into functions

--- chatbot/test_together_api.py
+++ chatbot/test_together_api.py
@@ -45,27 +45,65 @@ ENCOUNTERS:
 --------------------------------------------------------------------------------
 '''
 
-api_key = "29e5fa35b999a845ea50daaa50dd7a91f636011d9e111c40671d29495c41cf9d"
+TOGETHER_API_KEY = "29e5fa35b999a845ea50daaa50dd7a91f636011d9e111c40671d29495c41cf9d"
 
-TOGETHER_API_KEY = api_key
+def get_summary(api_key, user_ehr):
+    client = OpenAI(api_key=api_key,
+        base_url='https://api.together.xyz',
+    )
 
-client = OpenAI(api_key=TOGETHER_API_KEY,
-  base_url='https://api.together.xyz',
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {
+        "role": "system",
+        "content": "You are an AI assistant providing the user with a summary of their electronic health record. You are not a physician or a person of authority. You are given this information about the patient: "+user_ehr,
+        },
+        {
+        "role": "user",
+        "content": "Create a summary-style report for this patient that describes this patient's current health condition with the aim of educating the patient about their health. Format the report using bullet points and concise language for increased readability. At the end of the report, include a section of recommendations for actions the patient should take, but emphasize that the patient should communicate with their physician. Title the report 'Current Health Report for [patient]' replacing [patient] with the Patient's first and last name.",
+        }
+    ],
+    model="codellama/CodeLlama-13b-Instruct-hf",
+    max_tokens=1024)
+
+    return chat_completion.choices[0].message.content
+
+# append question to chat history prior to calling function
+def get_answer(api_key, user_summary, question, chat_history):
+    client = OpenAI(api_key=api_key,
+        base_url='https://api.together.xyz',
+    )
+
+    if len(chat_history)==0:
+        chat_history = [
+            {
+                "role": "system",
+                "content": "You are an AI assistant answering questions the user asks about their health. You are not a physician or a person of authority. You are given this information about the patient: "+user_summary,
+            },
+            {
+                "role": "user",
+                "content": "I am this patient. Please answer this question in detail: "+question,
+            }
+        ]
+    else:
+        chat_history.append( 
+             {"role":"user", "content": "I am this patient. "+question}
+        )
+
+    chat_completion = client.chat.completions.create(
+    messages=chat_history,
+    model="codellama/CodeLlama-13b-Instruct-hf",
+    max_tokens=1024)
+
+    return chat_completion.choices[0].message.content
+
+summary = get_summary(api_key=TOGETHER_API_KEY, user_ehr=user_ehr)
+print(summary)
+chat = []
+print(get_answer(
+    api_key=TOGETHER_API_KEY, 
+    user_summary=summary, 
+    question=input("Ask me about your health conditions!\n"),
+    chat_history=chat
+    )
 )
-
-chat_completion = client.chat.completions.create(
-  messages=[
-    {
-      "role": "system",
-      "content": "You are an AI assistant providing the user with a summary of their electronic health record. You are not a physician or a person of authority. You are given this information about the patient: "+user_ehr,
-    },
-    {
-      "role": "user",
-      "content": "Create a summary-style report for this patient that describes this patient's current health condition with the aim of educating the patient about their health. Format the report using bullet points and concise language for increased readability. At the end of the report, include a section of recommendations for actions the patient should take, but emphasize that the patient should communicate with their physician. Title the report 'Current Health Report for [patient]' replacing [patient] with the Patient's first and last name.",
-    }
-  ],
-  model="codellama/CodeLlama-13b-Instruct-hf",
-  max_tokens=1024
-)
-
-print(chat_completion.choices[0].message.content)
