089f6538c06157947d2c1b7d044831fa7450db36
YheChen <yanzhenchen123@gmail.com>
2024-02-17 16:14:10 -0800

    front-end initial

---
+++ treehacks 2/react-login-demo/frontend/src/home.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { useNavigate } from "react-router-dom";
+
+const Home = (props) => {
+    const { loggedIn, email } = props
+    const navigate = useNavigate();
+    
+    const onButtonClick = () => {
+        if (loggedIn) {
+            localStorage.removeItem("user")
+            props.setLoggedIn(false)
+        } else {
+            navigate("/login")
+        }
+    }
+
+    return (
+    
+        <div>
+
+<div className="nav"></div>
+        
+    <div className="mainContainer">
+        
+        <div className={"titleContainer"}>
+            <div className="welcomeheader">Welcome!</div>
+        </div>
+        <div className={"buttonContainer"}>
+            <input
+                className={"inputButton"}
+                type="button"
+                onClick={onButtonClick}
+                value={loggedIn ? "Log out" : "Log in"} />
+            {(loggedIn ? <div>
+                Your email address is {email}
+            </div> : <div/>)}
+        </div>
+        <div class="footer"></div>
+    </div>
+    </div>
+    )
+}
+
+export default Home
