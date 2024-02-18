8bde61b086392613ad01117ac0440c5a923d2fec
YheChen <yanzhenchen123@gmail.com>
2024-02-17 23:48:24 -0800

    removed unneeded clerk import, combined 2 lines

--- treehacks 2/react-login-demo/frontend/src/home.js
+++ treehacks 2/react-login-demo/frontend/src/home.js
@@ -1,6 +1,4 @@
-import React from "react"
-import { SignedIn, SignedOut, SignInButton, SignOutButton, useUser } from '@clerk/clerk-react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Home({ email }) {
